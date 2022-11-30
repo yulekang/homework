d0b60db3ea0b4906b7903e1a40520c1661c72d9a
yulekang <119045556+yulekang@users.noreply.github.com>
2022-11-30 18:25:59 +0100

    Add files via upload
    Make my repository pip installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="snowflake",
+    version="0.1",
+    author="DSSS",
+    author_email="yule.kang@fau.de",
+    packages=find_packages(),
+    install_requires=["numpy" , "turtle"]
+)
